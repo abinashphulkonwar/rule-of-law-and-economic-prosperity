4707e835ac829b8176c85c6eaef49438a9d0a43e
abinash <abinashphulkonwar1@gmail.com>
2025-04-21 12:59:34 +0530

    add experiment with trade openness as control variable

--- tests/3/test.py
+++ tests/3/test.py
@@ -7,15 +7,38 @@ import matplotlib.cm as cm
 import matplotlib.colors as mcolors
 
 
-df = pd.read_csv("./rl-pci.csv")
 
-df['log_percapita'] = np.log(df['percapita'])
 
+df = pd.read_csv("./rl-pci-control.csv")
+
+df['log_percapita'] = np.log(df['per capita'])
+df["trade_openness"] = (df["exports"] + df["imports"]) / df["gdp"]
 df_dummies = pd.get_dummies(df, columns=['year'], prefix='year', drop_first=True)  
 dummy_cols = ' + '.join(df_dummies.columns[df_dummies.columns.str.startswith('year_')])
 
 
-equation = f'log_percapita ~ estimate + {dummy_cols}'
+equation = f'log_percapita ~ estimate + trade_openness + {dummy_cols}'
 
 
 y, X = dmatrices(equation, data=df_dummies, return_type='dataframe')
+
+
+model = sm.OLS(y, X).fit(cov_type='HC3')
+
+
+# Save the summary to a text file
+with open("results.txt", "w") as f:
+    f.write(model.summary().as_text())
+summary_df = pd.DataFrame(model.summary().tables[1].data[1:], columns=model.summary().tables[1].data[0])
+summary_df.to_csv("results.csv", index=False)
+
+
+print("data size: 0.5 ", sm.stats.linear_rainbow(model))
+
+
+dy_col = dummy_cols.split("+")
+col_names = ["trade_openness"]
+for x in dy_col:
+  col_names.append(x.lstrip().rstrip())
+sm.graphics.plot_partregress('log_percapita', 'estimate', col_names, data=df_dummies, obs_labels=False)
+plt.show()
